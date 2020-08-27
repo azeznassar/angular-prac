668899f0d630239fed67f865b527d426fbc2c114
Azez Nassar <azez.nassar@outlook.com>
2020-08-27 17:47:00 +1200

    learn components/data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,14 +4,33 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers', // Element is typically used for components, not classes or attributes
   //selector: '[app-servers]',  -- To select by attribute: <div app-servers><div> instead of <app-servers></app-servers>
   //selector: '.app-servers', -- To select by class: <div class="app-servers"><div> instead of <app-servers></app-servers>
-  template: `<app-server></app-server><app-server></app-server>`, // inline template instead of linking to html file  - Templates must be present for all components
+  //template: `<app-server></app-server><app-server></app-server>`, // inline template instead of linking to html file  - Templates must be present for all components
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created";
+  serverName = "Testserver";
 
-  constructor() { }
+  constructor() { 
+
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created! The name is " + this.serverName;
+
+
+  }
+
+  OnUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
