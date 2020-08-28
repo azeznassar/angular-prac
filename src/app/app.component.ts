b966edb0e829b493b0af1429219876f939af0461
Azez Nassar <azez.nassar@outlook.com>
2020-08-28 16:47:50 +1200

    further learning of directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.scss']
-  styles: [`
-    h2 {
-      color: dodgerblue;
-  }
-  `]
+  styleUrls: ['./app.component.scss']
+  // styles: [`
+  //   h2 {
+  //     color: dodgerblue;
+  // }
+  // `]
 })
 export class AppComponent {
   name = 'Azez';
+  showMessage = false;
+  log = []
+
+  onToggleMessage() {
+    this.showMessage = !this.showMessage;
+    // this.log.push(this.log.length + 1)
+    this.log.push(new Date())
+
+  }
 }
