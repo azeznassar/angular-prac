dde4f9743f4951f5aa76261df1e7179fb0654266
Azez Nassar <azez.nassar@outlook.com>
2020-08-28 16:24:06 +1200

    learn to use builtin directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,13 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 export class ServerComponent {
     serverId: number = 5;
     serverStatus: string = "offline";
 
+    constructor() { // Built in method that every TS class has, which is called once the component is created
+        this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
+    }
+
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === "online" ? "green" : "red";
     }
 }
